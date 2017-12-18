4dc429084506ff4aef5ce2ae6e3935c81508bb14
Frank Poetzsch-Heffter <p-h@katharineum.de>
2017-12-18 15:31:11 +0100

    header footer termplates

--- schoolapps/todos/urls.py
+++ schoolapps/todos/urls.py
@@ -4,5 +4,6 @@ from .import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('details/<int:todo_id>/', views.details)
 ]
--- schoolapps/todos/views.py
+++ schoolapps/todos/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Todo
+
 def index(request):
-    return HttpResponse('hello World')
+    todos = Todo.objects.all()[:10]
+
+    context = {
+        'todos': todos  
+    }
+    return render(request, 'index.html', context)
+
+def details(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    context = {
+        'todo': todo
+    }
+    return render(request, 'details.html', context)
