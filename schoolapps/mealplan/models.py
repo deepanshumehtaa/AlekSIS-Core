93cb054e963f988090151cd814f248970d868562
HanseGucker <joniweth@gmx.de>
2018-11-29 17:48:16 +0100

    Rename files on upload

--- schoolapps/mealplan/models.py
+++ schoolapps/mealplan/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-
 # Create your models here.
+from helper import path_and_rename
+
+
 class MealPlan(models.Model):
     calendar_week = models.IntegerField()
     year = models.IntegerField()
-    pdf = models.FileField(upload_to="menus/")
+    pdf = models.FileField(upload_to=path_and_rename)
 
     def __str__(self):
         return "KW {}/{}".format(self.calendar_week, self.year)
