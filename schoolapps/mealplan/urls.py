22dc8ef53e044f876d3be27dd1956bc9423bfca3
HanseGucker <joniweth@gmx.de>
2018-12-04 17:34:44 +0100

    Show current menu

--- schoolapps/mealplan/urls.py
+++ schoolapps/mealplan/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="menu_index"),
     path('upload/', views.upload, name="menu_upload"),
-    path('<str:msg>', views.index, name="menu_index_msg")
+    path('current.pdf', views.show_current, name="menu_show_current"),
+    path('<str:msg>', views.index, name="menu_index_msg"),
 ]
--- schoolapps/mealplan/views.py
+++ schoolapps/mealplan/views.py
@@ -1,4 +1,9 @@
+import datetime
+import os
+
+from django.http import FileResponse
 from django.shortcuts import render, redirect
+from django.utils import timezone
 
 from mealplan.models import MealPlan
 from .forms import MenuUploadForm
@@ -22,3 +27,39 @@ def upload(request):
 def index(request, msg=None):
     menus = MealPlan.objects.all().order_by("calendar_week", "year")
     return render(request, 'menu/index.html', {"msg": msg, "menus": menus})
+
+
+def return_pdf(filename):
+    print(filename)
+    # Read and response PDF
+    file = open(filename, "rb")
+    return FileResponse(file, content_type="application/pdf")
+
+
+def return_default_pdf():
+    return return_pdf(os.path.join("mealplan", "default.pdf"))
+
+
+def show_current(request):
+    current_date = timezone.datetime.now()
+    year, calendar_week = current_date.isocalendar()[:2]
+    # print(calendar_week)
+    # current_date += datetime.timedelta(days=4)
+    days_to_add = 5 - current_date.isoweekday()
+    # print(days_to_add)
+    if days_to_add < 0:
+        days_to_add = days_to_add + 7
+    # print(days_to_add)
+    friday = current_date + datetime.timedelta(days=days_to_add)
+    # print(friday)
+    friday_14_10 = timezone.datetime(friday.year, friday.month, friday.day, 14, 10)
+    # print(friday_14_10)
+    if current_date > friday_14_10:
+        calendar_week += 1
+    print(calendar_week)
+    try:
+        obj = MealPlan.objects.get(year=year, calendar_week=calendar_week)
+        print(os.path.join("media", str(obj.pdf)))
+        return return_pdf(os.path.join("media", str(obj.pdf)))
+    except MealPlan.DoesNotExist:
+        return return_default_pdf()
