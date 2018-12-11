2ef81c5e2f87f7f33340610b49f148b916952e5c
HanseGucker <joniweth@gmx.de>
2018-12-11 16:55:21 +0100

    Add unique index to menu

--- schoolapps/mealplan/models.py
+++ schoolapps/mealplan/models.py
@@ -5,9 +5,14 @@ from helper import path_and_rename
 
 
 class MealPlan(models.Model):
-    calendar_week = models.IntegerField()
-    year = models.IntegerField()
-    pdf = models.FileField(upload_to=path_and_rename)
+    calendar_week = models.IntegerField(verbose_name="KW")
+    year = models.IntegerField(verbose_name="Jahr")
+    pdf = models.FileField(upload_to=path_and_rename, verbose_name="PDF")
+
+    class Meta:
+        unique_together = ("calendar_week", "year")
+        verbose_name = "Speiseplan"
+        verbose_name_plural = "Speisepläne"
 
     def __str__(self):
         return "KW {}/{}".format(self.calendar_week, self.year)
