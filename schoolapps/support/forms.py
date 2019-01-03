f3ca8dac6da62c148d00994f03919a2f2b0e84e9
Hangzhi <hangzhi@protonmail.com>
2019-01-03 11:52:45 +0100

    form in django without backend

---
+++ schoolapps/support/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from material import Layout, Row, Fieldset
+
+class REBUSForm(forms.Form):
+    categories = [('n1', 'Kein WLAN'), ('h1a', 'Beamer funktioniert nicht'), ('h1b', 'Fernseher funktioniert nicht'),
+                  ('h2a', 'PC funktioniert nicht'), ('h2b', 'Laptop funktioniert nicht')]
+
+    room = forms.CharField(label='Ihr Raum', max_length=10, required=True)
+    category = forms.ChoiceField(label='Kategorie', choices=categories, required=True)
+    short_description = forms.CharField(label='Bitte beschreiben Sie Ihren Fehler in einem Satz', required=True)
+    long_description = forms.CharField(label='Bitte beschreiben Sie Ihren Fehler genauer', required=False)
--- schoolapps/support/views.py
+++ schoolapps/support/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .forms import REBUSForm
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'support/index.html')
+    if request.method == 'POST':
+        form = REBUSForm(request.POST)
+        if form.is_valid():
+            # process data
+            return HttpResponseRedirect('support/rebus_submitted/')
+    else:
+        form = REBUSForm()
+
+    return render(request, 'support/rebus.html', {'form': form})
