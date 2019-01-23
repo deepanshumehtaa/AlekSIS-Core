3371fa4f8477e9c1b9bf4b860b2501b3bfdc717a
Dominik George <dominik.george@teckids.org>
2019-01-23 18:07:50 +0100

    Install django-simple-menu.

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     install_requires=[
                       'Django >= 2.0',
                       'django-bootstrap3',
+                      'django-simple-menu',
                       'django-tables2',
                      ],
     extras_requires={
