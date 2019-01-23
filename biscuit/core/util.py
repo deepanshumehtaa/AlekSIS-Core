697e7043e33610afb759a3f2f472c441ff061873
Dominik George <dominik.george@teckids.org>
2019-01-23 23:15:13 +0100

    Add documentation to util function.

--- biscuit/core/util.py
+++ biscuit/core/util.py
@@ -1,6 +1,9 @@
 import pkgutil
+from typing import Sequence
 
-def get_app_packages():
+def get_app_packages() -> Sequence:
+    """ Find all packages within the biscuit.apps namespace. """
+
     import biscuit.apps
     pkgs = ['biscuit.apps.%s' % i[1] for i in pkgutil.iter_modules(biscuit.apps.__path__)]
 
