78cbdf233da7fa4b5f88f108b080fdc87f1c88f0
Dominik George <dominik.george@teckids.org>
2019-01-23 18:19:57 +0100

    Automatically discover and mount BiscuIT app URLs.

--- biscuit/core/urls.py
+++ biscuit/core/urls.py
@@ -1,21 +1,16 @@
-"""core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.apps import apps
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Automatically mount URLs from all installed BiscuIT apps
+for app_config in apps.app_configs:
+    if not app.startswith('biscuit.apps.'):
+        continue
+
+    urlpatterns += path('%s/' % app_config.label, include('%s.urls' % app_config.name))
