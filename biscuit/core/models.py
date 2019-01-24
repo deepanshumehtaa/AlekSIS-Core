78111b73397bbd113d6c460c20c874e60ffab356
Dominik George <dominik.george@teckids.org>
2019-01-24 23:11:19 +0100

    Add string representation for persons.

--- biscuit/core/models.py
+++ biscuit/core/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Person(AbstractUser):
     is_student = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '%s, %s' % (self.last_name, self.first_name)
