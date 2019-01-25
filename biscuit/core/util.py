274a2e19dbec329a6208c44ea1818b15f6cdf458
Dominik George <dominik.george@teckids.org>
2019-01-26 00:57:09 +0100

    Allow starting without installed apps.

--- biscuit/core/util.py
+++ biscuit/core/util.py
@@ -4,7 +4,12 @@ from typing import Sequence
 def get_app_packages() -> Sequence:
     """ Find all packages within the biscuit.apps namespace. """
 
-    import biscuit.apps
+    # Import error are non-fatal here because probably simply no app is installed.
+    try:
+        import biscuit.apps
+    except ImportError:
+        return []
+
     pkgs = ['biscuit.apps.%s' % i[1] for i in pkgutil.iter_modules(biscuit.apps.__path__)]
 
     return pkgs
