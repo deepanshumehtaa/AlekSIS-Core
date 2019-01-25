4de3b9414be27b03acc8d905254bc7af3644a5d9
Dominik George <dominik.george@teckids.org>
2019-01-25 15:39:01 +0100

    Use local settings for database.

--- biscuit/core/settings.py
+++ biscuit/core/settings.py
@@ -70,8 +70,8 @@ WSGI_APPLICATION = 'biscuit.core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': LocalSetting(default='django.db.backends.sqlite3'),
+        'NAME': LocalSetting(default='{{ BASE_DIR }}/db.sqlite3'),
     }
 }
 
