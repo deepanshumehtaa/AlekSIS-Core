a58efc0a6593999ab3e51f16e0d59e18ee76e2f7
HanseGucker <joniweth@gmx.de>
2019-01-31 18:15:16 +0100

    Work at feedback / Refactor email templating

--- schoolapps/support/forms.py
+++ schoolapps/support/forms.py
@@ -3,13 +3,6 @@ from material import Layout, Row, Fieldset
 
 
 class REBUSForm(forms.Form):
-    # categories = [('Kein WLAN', 'Kein WLAN'), ('Beamer funktioniert nicht', 'Beamer funktioniert nicht'),
-    #               ('Fernseher funktioniert nicht', 'Fernseher funktioniert nicht'),
-    #               ('PC funktioniert nicht', 'PC funktioniert nicht'),
-    #               ('Laptop funktioniert nicht', 'Laptop funktioniert nicht'), ('Sonstiges', 'Sonstiges')]
-
-    # room = forms.CharField(label='Ihr Raum', max_length=15, required=True)
-    # category = forms.ChoiceField(label='Kategorie', choices=categories, required=True)
     a = forms.CharField(label="Kategorie A", required=True)
     b = forms.CharField(label="Kategorie B", required=False)
     c = forms.CharField(label="Kategorie C", required=False)
@@ -24,34 +17,26 @@ class FeedbackForm(forms.Form):
                (7, 7), (8, 8), (9, 9),
                (10, 10)]
 
-    design_rating = forms.ChoiceField(label='Bitte bewerten Sie das Design der SchoolApps auf einer Skala von 1 bis 10',
+    design_rating = forms.ChoiceField(label='Bitte bewerte das Design von SchoolApps auf einer Skala von 1 bis 10',
                                       choices=ratings, required=True)
-    functions_rating = forms.ChoiceField(label='Bitte bewerten Sie die Funktionen der SchoolApps auf einer Skala von '
-                                               '1 bis 10',
-                                         choices=ratings, required=True)
-    performance_rating = forms.ChoiceField(label='Bitte bewerten Sie die Geschwindigkeit der SchoolApps auf einer '
+    performance_rating = forms.ChoiceField(label='Bitte bewerte die Geschwindigkeit von SchoolApps auf einer '
                                                  'Skala von 1 bis 10',
                                            choices=ratings, required=True)
-    compatibility_rating = forms.ChoiceField(label='Bitte bewerten Sie die Kompatibilität der SchoolApps auf einer '
-                                                   'Skala von 1 bis 10',
-                                             choices=ratings, required=True)
-    usability_rating = forms.ChoiceField(label='Bitte bewerten Sie die Benutzerfreundlichkeit der SchoolApps auf '
+    usability_rating = forms.ChoiceField(label='Bitte bewerte die Benutzerfreundlichkeit von SchoolApps auf '
                                                'einer Skala von 1 bis 10',
                                          choices=ratings, required=True)
-    overall_rating = forms.ChoiceField(label='Bitte bewerten Sie die SchoolApps insgesamt auf einer Skala von 1 bis 10',
+    overall_rating = forms.ChoiceField(label='Bitte bewerte SchoolApps insgesamt auf einer Skala von 1 bis 10',
                                        choices=ratings, required=True)
-    short_description = forms.CharField(label='Bitte geben Sie ein kurzes Feedback in einem Satz ein', required=True)
-    long_description = forms.CharField(label='Bitte geben Sie ein ausführliches Feedback ein', required=False)
-    ideas = forms.CharField(label='Haben Sie Ideen, die wir in die SchoolApps einbauen können? Geben Sie sie hier ein!',
-                            required=False)
 
-    layout = Layout(Row('design_rating'),
-                    Row('functions_rating'),
-                    Row('performance_rating'),
-                    Row('compatibility_rating'),
-                    Row('usability_rating'),
+    apps = forms.CharField(label="Bitte gebe uns Feedback zu den einzelnen Funktion von SchoolApps", required=False,
+                           widget=forms.Textarea)
+    more = forms.CharField(label="Möchtest du uns sonst noch etwas mitteilen?", required=False, widget=forms.Textarea)
+    ideas = forms.CharField(label='Hast du Ideen, was wir noch in SchoolApps einbauen könnten/sollten?',
+                            required=False, widget=forms.Textarea)
+
+    layout = Layout(Row('design_rating', 'performance_rating', 'usability_rating'),
                     Row('overall_rating'),
-                    Row('short_description'),
-                    Row('long_description'),
+                    Row("apps"),
                     Row('ideas'),
+                    Row('more'),
                     )
--- schoolapps/support/views.py
+++ schoolapps/support/views.py
@@ -20,18 +20,19 @@ def rebus(request):
     if request.method == 'POST':
         form = REBUSForm(request.POST)
         if form.is_valid():
-            # room = form.cleaned_data['room']
+            # Read out form data
             contraction = request.user.username
             a = form.cleaned_data['a']
             b = form.cleaned_data["b"]
             c = form.cleaned_data["c"]
             short_description = form.cleaned_data['short_description']
             long_description = form.cleaned_data['long_description']
+
+            # Build description for kanboard
             description = "**Kategorie:** {} > {} > {} \n\n **Übermittelt von:** {} \n\n **Nachricht:** {}".format(a, b,
                                                                                                                    c,
                                                                                                                    contraction,
                                                                                                                    long_description)
-            # description = "**Kategorie: **" + a + "\n\n" + "**Raum: **\n\n**Übermittelt von: **" + contraction + "\n\n" + "**Nachricht: **" + long_description + "\n\n"
             # Add kanboard task
             kb.create_task(project_id=p_id_rebus, title=short_description, description=description)
 
@@ -40,6 +41,8 @@ def rebus(request):
             act = Activity(title="Du hast uns ein Problem gemeldet.", description=desc_act, app="REBUS",
                            user=request.user)
             act.save()
+
+            # Send mail
             context = {
                 "a": a,
                 "b": b,
@@ -48,8 +51,8 @@ def rebus(request):
                 "long_desc": long_description,
                 "user": request.user.username
             }
-            send_mail_with_template("Neue REBUS-Meldung", ["support@katharineum.de"], "support/mail/email.txt",
-                                    "support/mail/email.html", context)
+            send_mail_with_template("Neue REBUS-Meldung", ["support@katharineum.de"], "support/mail/rebus.txt",
+                                    "support/mail/rebus.html", context)
 
             return render(request, 'support/rebus_submitted.html')
     else:
@@ -62,26 +65,62 @@ def feedback(request):
     if request.method == 'POST':
         form = FeedbackForm(request.POST)
         if form.is_valid():
+            # Read out form data
             design_rating = form.cleaned_data['design_rating']
-            functions_rating = form.cleaned_data['functions_rating']
             performance_rating = form.cleaned_data['performance_rating']
-            compatibility_rating = form.cleaned_data['compatibility_rating']
             usability_rating = form.cleaned_data['usability_rating']
             overall_rating = form.cleaned_data['overall_rating']
-            short_description = form.cleaned_data['short_description']
-            long_description = form.cleaned_data['long_description']
+            more = form.cleaned_data['more']
             ideas = form.cleaned_data['ideas']
-            description = "**Bewertung fürs Design: **" + design_rating + "\n\n" + "**Bewertung für die Funktionen: **" + functions_rating + "\n\n" + "**Bewertung für die Performance: **" + performance_rating + "\n\n" + "**Bewertung für die Kompatibilität: **" + compatibility_rating + "\n\n" + "**Bewertung für die Benutzerfreundlichkeit: **" + usability_rating + "\n\n" + "**GESAMTBEWERTUNG: **" + overall_rating + "\n\n" + "**Nachricht: **" + long_description + "\n\n" + "**IDEEN: **" + ideas + "\n\n"
+            apps = form.cleaned_data["apps"]
 
+            # Build description for kanboard
+            description = """
+            **Bewertungen:** {}/10 (Design), {}/10 (Geschwindigkeit), {}/10 (Benutzerfreundlichkeit)
+            
+            **Bewertung (insgesamt):** {}/10
+            
+            **Feedback zu einzelnen Apps:** {}
+            
+            **Ideen/Wünsche:** {}
+            
+            **Sonstiges:** {}
+            """.format(design_rating, performance_rating, usability_rating, overall_rating, apps, ideas, more)
+
+            # Get color for kanboard by rating
             if int(overall_rating) <= 3:
-                kb.create_task(project_id=p_id_feedback, title=short_description, description=description,
-                               color_id='red')
+                color = "red"
             elif 3 < int(overall_rating) <= 7:
-                kb.create_task(project_id=p_id_feedback, title=short_description, description=description,
-                               color_id='yellow')
+                color = "yellow"
             else:
-                kb.create_task(project_id=p_id_feedback, title=short_description, description=description,
-                               color_id='green')
+                color = "green"
+
+            # Add kanboard task
+            kb.create_task(project_id=p_id_feedback,
+                           title="Feedback von {}".format(request.user.username),
+                           description=description,
+                           color_id=color)
+
+            # Register activity
+            act = Activity(title="Du hast uns Feedback gegeben.",
+                           description="Du hast uns auf einer Skala von 1 bis 10 mit {} bewertet.".format(
+                               overall_rating), app="Feedback",
+                           user=request.user)
+            act.save()
+
+            # Send mail
+            context = {
+                "design": design_rating,
+                "performance": performance_rating,
+                "usability": usability_rating,
+                "overall": overall_rating,
+                "more": more,
+                "apps": apps,
+                "ideas": ideas,
+                "user": request.user.username
+            }
+            send_mail_with_template("Neues Feedback", ["support@katharineum.de"], "support/mail/feedback.txt",
+                                    "support/mail/feedback.html", context)
 
             return render(request, 'support/feedback_submitted.html')
     else:
