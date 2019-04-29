9b155b4091248e76ba1d36103a7510d80c9c311b
Julian Leucker <leuckerj@gmail.com>
2019-04-29 21:27:21 +0200

    Update the model (question/answer in one model

--- schoolapps/faq/admin.py
+++ schoolapps/faq/admin.py
@@ -1,22 +1,14 @@
 from django.contrib import admin
-from django import forms
-from faq.models import Question, FAQQuestion, FAQAnswer
+from faq.models import Question, FAQQuestion
 
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ("question_text", "pub_date", "user", "answered")
+    list_filter = ("answered",)
 
 admin.site.register(Question, QuestionAdmin)
 
-# class FAQQuestionForm(forms.ModelForm):
-#     question_text = forms.CharField(widget=forms.Textarea)
-#
-#     class Meta:
-#         model = FAQQuestion
-#
 class FAQQuestionAdmin(admin.ModelAdmin):
-    #form = FAQQuestionForm
     list_display = ("question_text", "icon", "answered")
 
 admin.site.register(FAQQuestion, FAQQuestionAdmin)
-admin.site.register(FAQAnswer)
--- schoolapps/faq/models.py
+++ schoolapps/faq/models.py
@@ -2,22 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class FAQAnswer(models.Model):
-    answer_text = models.TextField()
-
-    def __str__(self):
-        return self.answer_text
-
-    class Meta:
-        verbose_name = "FAQ-Antwort"
-        verbose_name_plural = "FAQ-Antworten"
-
 class FAQQuestion(models.Model):
     question_text = models.TextField()
     icon = models.CharField(max_length=20, blank=True, default="question_answer")
 
     answered = models.BooleanField(verbose_name="Beantwortet", default=False)
-    answer = models.ForeignKey(FAQAnswer, on_delete=models.CASCADE, blank=True)
+    answer_text = models.TextField(blank=True)
 
     def __str__(self):
         return self.question_text
