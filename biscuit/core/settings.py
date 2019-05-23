e739798a8cfbaa1a1a136858b4aebceae80b037d
Dominik George <dominik.george@teckids.org>
2019-05-23 23:24:49 +0200

    Have bootstrap3 include jquery.

--- biscuit/core/settings.py
+++ biscuit/core/settings.py
@@ -108,6 +108,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+BOOTSTRAP3 = {
+    'include_jquery': True
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
