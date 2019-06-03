b4ba5eccbce4edd1c82a6f2462a6da18cc44a00f
Julian Leucker <leuckerj@gmail.com>
2019-06-03 11:18:42 +0200

    fixed typo (failback → fallback)

--- schoolapps/timetable/fallback_view.py
+++ schoolapps/timetable/fallback_view.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 @login_required
 def fallback(request, *args, **kwargs):
-    return render(request, "timetable/failback.html")
+    return render(request, "timetable/fallback.html")
