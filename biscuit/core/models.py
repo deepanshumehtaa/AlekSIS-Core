4b8bbd3cb64b7d81d5d8781be37503d791d96e36
Dominik George <dominik.george@teckids.org>
2019-07-15 12:58:39 +0200

    Move choices to model.

--- biscuit/core/models.py
+++ biscuit/core/models.py
@@ -2,13 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-SEX_CHOICES = [
-    ('f', _('female')),
-    ('m', _('male'))
-]
-
 
 class Person(AbstractUser):
+    SEX_CHOICES = [
+        ('f', _('female')),
+        ('m', _('male'))
+    ]
+
     additional_name = models.CharField(max_length=30, blank=True)
 
     street = models.CharField(max_length=30, blank=True)
