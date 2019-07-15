8fc7f610ae30534e2ec57b7a9a17f94774c4f25b
Dominik George <dominik.george@teckids.org>
2019-07-15 14:51:19 +0200

    Remove decoratrs for legacy role fields.

--- biscuit/core/decorators.py
+++ biscuit/core/decorators.py
@@ -1,16 +1,6 @@
 from django.contrib.auth.decorators import user_passes_test
 
 
-def student_required(function=None):
-    actual_decorator = user_passes_test(lambda u: u.is_active and u.is_student)
-    return actual_decorator(function)
-
-
-def teacher_required(function=None):
-    actual_decorator = user_passes_test(lambda u: u.is_active and u.is_teacher)
-    return actual_decorator(function)
-
-
 def admin_required(function=None):
     actual_decorator = user_passes_test(
         lambda u: u.is_active and u.is_superuser)
