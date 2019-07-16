97d3e9dfb682d1b6ce23df3f51bd09e156897723
Tom Teichler <tom.teichler@teckids.org>
2019-07-17 01:06:04 +0200

    Implement view for person. Closes #10.

--- biscuit/core/views.py
+++ biscuit/core/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.utils.translation import gettext as _
+from django.http import Http404
+from django.shortcuts import redirect, render
 from django_tables2 import RequestConfig
+from django.urls import reverse
 from .models import Person
 from .tables import PersonsTable
 
@@ -21,3 +24,18 @@ def persons(request):
     context['persons_table'] = persons_table
 
     return render(request, 'core/persons.html', context)
+
+@login_required
+def person(request, id_):
+    context = {}
+
+    # Get person and check access
+    try:
+        person = Person.objects.get(pk=id_)
+    except Person.DoesNotExist as e:
+        # Turn not-found object into a 404 error
+        raise Http404 from e
+
+    context['person'] = person
+
+    return render(request, 'core/person.html', context)
