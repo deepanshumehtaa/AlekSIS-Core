8969d687655e20a89e5804eda71d04d82660a5b0
Dominik George <dominik.george@teckids.org>
2019-07-17 13:34:08 +0200

    Allow passing optional args to messages framework.

--- biscuit/core/util/messages.py
+++ biscuit/core/util/messages.py
@@ -3,28 +3,28 @@ import logging
 from django.contrib import messages
 
 
-def add_message(request, level, message):
+def add_message(request, level, message, **kwargs):
     if request:
-        return messages.add_message(request, level, message)
+        return messages.add_message(request, level, message, **kwargs)
     else:
         return logging.getLogger(__name__).log(level, message)
 
 
-def debug(request, message):
-    return add_message(request, messages.DEBUG, message)
+def debug(request, message, **kwargs):
+    return add_message(request, messages.DEBUG, message, **kwargs)
 
 
-def info(request, message):
-    return add_message(request, messages.INFO, message)
+def info(request, message, **kwargs):
+    return add_message(request, messages.INFO, message, **kwargs)
 
 
-def success(request, message):
-    return add_message(request, messages.SUCCESS, message)
+def success(request, message, **kwargs):
+    return add_message(request, messages.SUCCESS, message, **kwargs)
 
 
-def warning(request, message):
-    return add_message(request, messages.WARNING, message)
+def warning(request, message, **kwargs):
+    return add_message(request, messages.WARNING, message, **kwargs)
 
 
-def error(request, message):
-    return add_message(request, messages.ERROR, message)
+def error(request, message, **kwargs):
+    return add_message(request, messages.ERROR, message, **kwargs)
