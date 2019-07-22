c00e5f1fa79d181df7d08cda544c71611af2d4e6
Julian Leucker <leuckerj@gmail.com>
2019-07-22 13:30:07 +0200

    Add help_text to faq-input (issue #209)

--- schoolapps/faq/models.py
+++ schoolapps/faq/models.py
@@ -12,7 +12,7 @@ class FAQQuestion(models.Model):
     icon = models.CharField(max_length=20, blank=True, default="question_answer")
 
     show = models.BooleanField(verbose_name="Veröffentlicht", default=False)
-    answer_text = models.TextField(blank=True)
+    answer_text = models.TextField(blank=True, help_text="Bei den Antworten funktioniert auch HTML-Syntax!<br> Aus Gründen des verwendeten CSS-Frameworks muss der Tag <strong>&lt;ul&gt;</strong> die CSS-Klasse <em>browser-default</em> besitzen!")
 
     # section = mo
 
