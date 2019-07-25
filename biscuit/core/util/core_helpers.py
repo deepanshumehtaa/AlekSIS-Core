02920eae374b5cf1413c761a93af90cd3c5d709c
Dominik George <dominik.george@teckids.org>
2019-07-25 22:20:38 +0200

    Add additional apps only once.

--- biscuit/core/util/core_helpers.py
+++ biscuit/core/util/core_helpers.py
@@ -16,7 +16,12 @@ def get_app_packages() -> Sequence:
     for pkg in pkgutil.iter_modules(biscuit.apps.__path__):
         mod = import_module('biscuit.apps.%s' % pkg[1])
 
-        pkgs += getattr(mod, 'INSTALLED_APPS', [])
-        pkgs += ['biscuit.apps.%s' % pkg[1]]
+        # Add additional apps defined in module's INSTALLED_APPS constant
+        additional_apps = getattr(mod, 'INSTALLED_APPS', [])
+        for app in additional_apps:
+            if not app in pkgs:
+                pkgs.append(app)
+
+        pkgs.append('biscuit.apps.%s' % pkg[1])
 
     return pkgs
