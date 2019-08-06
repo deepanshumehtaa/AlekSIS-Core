92334eb284fc9c54039cd508952647826ae9010d
Jonathan Weth <git@jonathanweth.de>
2019-08-06 11:37:20 +0200

    Some permission fixes

--- schoolapps/debug/views.py
+++ schoolapps/debug/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render
 
 from debug.models import DebugLogGroup
 
 
 @login_required
-# @permission_required("timetable.")
+@permission_required("debug.can_view_debug_log")
 def debugging_tool(request):
     groups = DebugLogGroup.objects.all()
     return render(request, "debug/debug.html", {"groups": groups})
