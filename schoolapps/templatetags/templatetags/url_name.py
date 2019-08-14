f8bb9393898882d9797dbf773d707ab247144a22
Jonathan Weth <git@jonathanweth.de>
2019-08-14 18:47:05 +0200

    Allow greater than character in LaTeX (issue #241)

--- schoolapps/templatetags/templatetags/url_name.py
+++ schoolapps/templatetags/templatetags/url_name.py
@@ -3,8 +3,10 @@ from django import template
 
 register = template.Library()
 
-def get_url_name(request): # Only one argument.
+
+def get_url_name(request):  # Only one argument.
     """Gets url_name"""
     return resolve(request.path_info).url_name
+
 
 register.filter("url_name", get_url_name)
