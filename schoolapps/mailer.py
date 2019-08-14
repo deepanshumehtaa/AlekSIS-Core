0681b49ba2e2414d252379c6326da223d65681a2
Jonathan Weth <git@jonathanweth.de>
2019-08-14 19:05:41 +0200

    Change email texts of support app to make them OTRS ready (issue #234)

--- schoolapps/mailer.py
+++ schoolapps/mailer.py
@@ -4,7 +4,7 @@ from django.template.loader import render_to_string
 SENDER_EMAIL = 'SchoolApps <infoplan@katharineum.de>'
 
 
-def send_mail_with_template(title, receivers, plain_template, html_template, context={}):
+def send_mail_with_template(title, receivers, plain_template, html_template, context={}, sender_email=SENDER_EMAIL):
     msg_plain = render_to_string(plain_template, context)
     msg_html = render_to_string(html_template, context)
 
@@ -12,7 +12,7 @@ def send_mail_with_template(title, receivers, plain_template, html_template, con
         send_mail(
             title,
             msg_plain,
-            SENDER_EMAIL,
+            sender_email,
             receivers,
             html_message=msg_html,
         )
