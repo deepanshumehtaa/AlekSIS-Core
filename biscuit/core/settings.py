ccd3455f10459e52822af09670fc589e0a8af01b
Dominik George <dominik.george@teckids.org>
2019-08-20 23:21:15 +0200

    Rewrite settings.py to use dynaconf.

--- biscuit/core/settings.py
+++ biscuit/core/settings.py
@@ -1,23 +1,37 @@
+from glob import glob
 import os
+import sys
 
-from local_settings import load_and_check_settings, LocalSetting, SecretSetting
+from dynaconf import LazySettings
 
 from .util.core_helpers import get_app_packages
+
+ENVVAR_PREFIX_FOR_DYNACONF = 'BISCUIT'
+DIRS_FOR_DYNACONF = ['/etc/biscuit']
+
+SETTINGS_FILE_FOR_DYNACONF = []
+for directory in DIRS_FOR_DYNACONF:
+    SETTINGS_FILE_FOR_DYNACONF += glob(os.path.join(directory, '*.ini'))
+    SETTINGS_FILE_FOR_DYNACONF += glob(os.path.join(directory, '*.yaml'))
+    SETTINGS_FILE_FOR_DYNACONF += glob(os.path.join(directory, '*.toml'))
+
+_settings = LazySettings(
+    ENVVAR_PREFIX_FOR_DYNACONF=ENVVAR_PREFIX_FOR_DYNACONF,
+    SETTINGS_FILE_FOR_DYNACONF=SETTINGS_FILE_FOR_DYNACONF
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SecretSetting()
+SECRET_KEY = _settings.get('secret_key', 'DoNotUseInProduction')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = LocalSetting(default=False)
+DEBUG = _settings.get('debug', False)
 
-ALLOWED_HOSTS = LocalSetting(default=[])
-
+ALLOWED_HOSTS = _settings.get('http_hosts', [])
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -45,12 +59,12 @@ STATICFILES_FINDERS = [
     'sass_processor.finders.CssFinder'
 ]
 
-SASS_PROCESSOR_AUTO_INCLUDE = False 
+SASS_PROCESSOR_AUTO_INCLUDE = False
 SASS_PROCESSOR_CUSTOM_FUNCTIONS = {
     'get-colour': 'biscuit.core.util.sass_helpers.get_colour',
 }
 SASS_PROCESSOR_INCLUDE_DIRS = [
-    LocalSetting(default='/usr/share/sass/bootstrap')
+    _settings.get('bootstrap.sass_path', '/usr/share/sass/bootstrap')
 ]
 
 MIDDLEWARE = [
@@ -91,8 +105,8 @@ WSGI_APPLICATION = 'biscuit.core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': LocalSetting(default='django.db.backends.sqlite3'),
-        'NAME': LocalSetting(default='{{ BASE_DIR }}/db.sqlite3'),
+        'ENGINE': _settings.get('database.engine', 'django.db.backends.sqlite3'),
+        'NAME': _settings.get('database.name', 'db.sqlite3'),
     }
 }
 
@@ -119,9 +133,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = LocalSetting(default='de-de')
+LANGUAGE_CODE = _settings.get('l10n.lang', 'de-de')
 
-TIME_ZONE = LocalSetting(default='Europe/Berlin')
+TIME_ZONE = _settings.get('l10n.tz', 'Europe/Berlin')
 
 USE_I18N = True
 
@@ -132,47 +146,47 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-
-MENU_SELECT_PARENTS = True
+STATIC_URL = _settings.get('static.url', '/static/')
+MEDIA_URL = _settings.get('media.url', '/media/')
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 
-STATIC_ROOT = LocalSetting()
-MEDIA_ROOT = LocalSetting()
+STATIC_ROOT = _settings.get('static.root')
+MEDIA_ROOT = _settings.get('media.root')
 
-FONT_AWESOME = {'url': LocalSetting(
-    default="/javascript/font-awesome/css/font-awesome.min.css")}
+FONT_AWESOME = {'url': _settings.get(
+    'bootstrap.fa_url', '/javascript/font-awesome/css/font-awesome.min.css')}
 
 BOOTSTRAP4 = {
-    "css_url": LocalSetting(default="/javascript/bootstrap4/css/bootstrap.min.css"),
-    "javascript_url": LocalSetting(default="/javascript/bootstrap4/js/bootstrap.min.js"),
-    "jquery_url": LocalSetting(default="/javascript/jquery/jquery.min.js"),
-    "popper_url": LocalSetting(default="/javascript/popper.js/umd/popper.min.js"),
-    "include_jquery": True,
-    "include_popper": True
+    'css_url': _settings.get('bootstrap.css_url', '/javascript/bootstrap4/css/bootstrap.min.css'),
+    'javascript_url': _settings.get('bootstrap.js_url', '/javascript/bootstrap4/js/bootstrap.min.js'),
+    'jquery_url': _settings.get('bootstrap.jquery_url', '/javascript/jquery/jquery.min.js'),
+    'popper_url': _settings.get('bootstrap.popper_url', '/javascript/popper.js/umd/popper.min.js'),
+    'include_jquery': True,
+    'include_popper': True
 }
+
+DATATABLES_BASE = _settings.get(
+    'bootstrap.datatables_base', '/javascript/jquery-datatables')
 
 ANY_JS = {
     'DataTables': {
-        'js_url': LocalSetting(default='/javascript/jquery-datatables/jquery.dataTables.min.js')
+        'js_url': DATATABLES_BASE + '/jquery.dataTables.min.js'
     },
     'DataTables-Bootstrap4': {
-        'css_url': LocalSetting(default="/javascript/jquery-datatables/css/dataTables.bootstrap4.min.css"),
-        'js_url': LocalSetting(default="/javascript/jquery-datatables/dataTables.bootstrap4.min.js")
+        'css_url': DATATABLES_BASE + '/css/dataTables.bootstrap4.min.css',
+        'js_url': DATATABLES_BASE + '/dataTables.bootstrap4.min.js'
     }
 }
 
-COLOUR_PRIMARY = LocalSetting(default='#007bff')
-COLOUR_SECONDARY = LocalSetting(default='#6c757d')
-COLOUR_SUCCESS = LocalSetting(default='#28a745')
-COLOUR_INFO = LocalSetting(default='#17a2b8')
-COLOUR_WARNING = LocalSetting(default='#ffc107')
-COLOUR_DANGER = LocalSetting(default='#dc3545')
-COLOUR_LIGHT = LocalSetting(default='#f8f9fa')
-COLOUR_DARK = LocalSetting(default='#343a40')
+COLOUR_PRIMARY = _settings.get('theme.colours.primary', '#007bff')
+COLOUR_SECONDARY = _settings.get('theme.colours.secondary', '#6c757d')
+COLOUR_SUCCESS = _settings.get('theme.colours.success', '#28a745')
+COLOUR_INFO = _settings.get('theme.colours.info', '#17a2b8')
+COLOUR_WARNING = _settings.get('theme.colours.warning', '#ffc107')
+COLOUR_DANGER = _settings.get('theme.colours.danger', '#dc3545')
+COLOUR_LIGHT = _settings.get('theme.colours.light', '#f8f9fa')
+COLOUR_DARK = _settings.get('theme.colours.dark', '#343a40')
 
-_settings = load_and_check_settings(globals())
-globals().update(_settings)
+_settings.populate_obj(sys.modules[__name__])
