a8d0112ef4669674c247e3727d703d26bd08a9aa
mirabilos <thorsten.glaser@teckids.org>
2019-08-21 16:43:46 +0200

    Work around multiple relative installations bug in poetry.
    cf. https://github.com/sdispater/poetry/issues/1123

--- dev.sh
+++ dev.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+remove_pip_metadata() {
+    find . -type d -name pip-wheel-metadata -print0 | xargs -0r rm -rf --
+}
+
 case "$1" in
     "install-all")
 	cd "$(dirname "$0")"
+	remove_pip_metadata
 	poetry install
 	for d in apps/official/*; do
+	    remove_pip_metadata
 	    poetry run sh -c "cd $d; poetry install"
 	done
+	remove_pip_metadata
 	poetry run ./manage.py migrate
 	;;
     *)
