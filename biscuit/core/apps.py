afb4fde683cf2fe859ffa69d0e85dfad8d4910da
Dominik George <dominik.george@teckids.org>
2019-08-21 17:18:47 +0200

    Clean up SCSS on startup.

---
+++ biscuit/core/apps.py
@@ -0,0 +1,22 @@
+from glob import glob
+import os
+
+from django.apps import AppConfig
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
+
+class CoreConfig(AppConfig):
+    name = 'biscuit.core'
+    verbose_name = _('BiscuIT - The Free School Information System')
+
+    def clean_scss(self):
+        for source_map in glob(os.path.join(settings.STATIC_ROOT, '*.css.map')):
+            try:
+                os.unlink(source_map)
+            except OSError:
+                # Ignore because old is better than nothing
+                pass  # noqa
+
+    def ready(self):
+        self.clean_scss()
