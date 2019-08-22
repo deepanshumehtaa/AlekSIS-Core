56e138e1a98adf33a2353010cb9d02a78c7c67a3
Dominik George <dominik.george@teckids.org>
2019-08-22 15:15:18 +0200

    Fix typing in util code.

--- biscuit/core/util/messages.py
+++ biscuit/core/util/messages.py
@@ -1,32 +1,32 @@
 import logging
-from typing import Optional
+from typing import Any, Optional
 
 from django.contrib import messages
 from django.http import HttpRequest
 
 
-def add_message(request: Optional[HttpRequest], level: int, message: str, **kwargs) -> Any:
+def add_message(request: Optional[HttpRequest], level: int, message: str, **kwargs) -> Optional[Any]:
     if request:
         return messages.add_message(request, level, message, **kwargs)
     else:
         return logging.getLogger(__name__).log(level, message)
 
 
-def debug(request: Optional[HttpRequest], message: str, **kwargs) -> Any
+def debug(request: Optional[HttpRequest], message: str, **kwargs) -> Optional[Any]
     return add_message(request, messages.DEBUG, message, **kwargs)
 
 
-def info(request: Optional[HttpRequest], message: str, **kwargs) -> Any
+def info(request: Optional[HttpRequest], message: str, **kwargs) -> Optional[Any]
     return add_message(request, messages.INFO, message, **kwargs)
 
 
-def success(request: Optional[HttpRequest], message: str, **kwargs) -> Any
+def success(request: Optional[HttpRequest], message: str, **kwargs) -> Optional[Any]
     return add_message(request, messages.SUCCESS, message, **kwargs)
 
 
-def warning(request: Optional[HttpRequest], message: str, **kwargs) -> Any
+def warning(request: Optional[HttpRequest], message: str, **kwargs) -> Optional[Any]
     return add_message(request, messages.WARNING, message, **kwargs)
 
 
-def error(request: Optional[HttpRequest], message: str, **kwargs) -> Any
+def error(request: Optional[HttpRequest], message: str, **kwargs) -> Optional[Any]
     return add_message(request, messages.ERROR, message, **kwargs)
