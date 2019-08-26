50911b98ab4e8229111231c2771680cafdfc16af
Tom Teichler <tom.teichler@teckids.org>
2019-08-26 20:09:11 +0200

    Add function to edit user. Closes #49, #14.

--- biscuit/core/forms.py
+++ biscuit/core/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
 
 from .models import Person
+from .utils import
 
 
 class PersonAccountForm(forms.ModelForm):
@@ -36,3 +37,29 @@ class PersonAccountForm(forms.ModelForm):
 
 PersonsAccountsFormSet = forms.modelformset_factory(
     Person, form=PersonAccountForm, max_num=0, extra=0)
+
+class EditPersonForm(forms.ModelForm)
+    class Meta:
+        model = Person
+        fields = ['user', 'is_active', 'first_name', 'last_name', 'additional_name', 'short_name', 'street', 'housenumber', 'postal_code', 'place', 'phone_number', 'mobile_number', 'email', 'date_of_birth', 'sex', 'photo']
+
+    new_user = forms.CharField(
+        required=False,
+        label=_('New user'),
+        help_text=_('Create a new account'),
+
+    def clean(self) -> None:
+        User = get_user_model()
+
+        if self.cleaned_data.get('new_user', None):
+            if self.cleaned_data.get('user', None):
+                self.add_error('new_user', _('You cannot set a new username when also selecting an existing user.'))
+            elif User.objects.filter(username=self.cleaned_data['new_user']).exists():
+                self.add_error('new_user', _('This username is already in use.'))
+            else:
+                new_user_obj = User.objects.create_user(self.cleaned_data['new_user'],
+                                                        self.instance.email,
+                                                        first_name=self.instance.first_name,
+                                                         last_name=self.instance.last_name)
+
+                self.cleaned_data['user'] = new_user_obj
