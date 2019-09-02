fc979f2b8329436d6826adc969739a2094aac29e
Dominik George <dominik.george@teckids.org>
2019-09-02 15:20:43 +0200

    Update translations and submodules.

--- dev.sh
+++ dev.sh
@@ -20,6 +20,15 @@ case "$1" in
 	poetry run ./manage.py compilemessages
 	poetry run ./manage.py collectstatic --no-input
 	;;
+    "makemessages")
+        cd "$(dirname "$0")"
+        manage_py=$(realpath manage.py)
+        locales="-l ar -l de_DE -l fr -l nb_NO -l tr_TR"
+        for d in biscuit/core apps/official/*/biscuit/apps/*; do
+        	echo; echo "Entering $d."
+        	poetry run sh -c "cd $d; $manage_py makemessages --no-wrap -i static $locales"
+        done
+        ;;
     *)
 	;;
 esac
