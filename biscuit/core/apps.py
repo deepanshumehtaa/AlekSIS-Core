63889dba535e79bb5d831ae37ce55ba8856ca6f2
Dominik George <dominik.george@teckids.org>
2019-09-05 22:34:18 +0200

    Fix app name

--- biscuit/core/apps.py
+++ biscuit/core/apps.py
@@ -3,12 +3,11 @@ import os
 
 from django.apps import AppConfig
 from django.conf import settings
-from django.utils.translation import ugettext as _
 
 
 class CoreConfig(AppConfig):
     name = 'biscuit.core'
-    verbose_name = _('BiscuIT - The Free School Information System')
+    verbose_name = 'BiscuIT - The Free School Information System'
 
     def clean_scss(self) -> None:
         for source_map in glob(os.path.join(settings.STATIC_ROOT, '*.css.map')):
