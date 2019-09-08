9bdc3b2f202de0b0e1ad20c40d9b5c32c354fc71
Dominik George <dominik.george@teckids.org>
2019-09-08 23:16:30 +0200

    Retry backup cronjob on failure.

--- biscuit/core/cronjobs.py
+++ biscuit/core/cronjobs.py
@@ -5,7 +5,9 @@ from django_cron import CronJobBase, Schedule
 
 class Backup(CronJobBase):
     RUN_AT_TIMES = settings.DBBACKUP_CRON_TIMES
-    schedule = Schedule(run_at_times=RUN_AT_TIMES)
+    RETRY_AFTER_FAILURE_MINS = 5
+
+    schedule = Schedule(run_at_times=RUN_AT_TIMES, retry_after_failure_mins=RETRY_AFTER_FAILURE_MINS)
     code = 'biscuit.core.Backup'
 
     def do(self):
