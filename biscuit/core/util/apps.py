5c4ec61cd8db5a260d4444286151080c91c66aeb
Dominik George <dominik.george@teckids.org>
2019-11-07 21:30:38 +0100

    Only load model_extensions for current app.
    (The loop was really bullshit…)

--- biscuit/core/util/apps.py
+++ biscuit/core/util/apps.py
@@ -9,10 +9,9 @@ class AppConfig(django.apps.AppConfig):
     def ready(self):
         super().ready()
 
-        # Run model extension code in all apps
-        for app in django.apps.get_app_configs():
-            try:
-                import_module('%s.model_extensions' % app.module.__name__)
-            except ImportError:
-                # ImportErrors are non-fatal because model extensions are optional.
-                pass
+        # Run model extension code
+        try:
+            import_module('%s.model_extensions' % self.__class__.__module__.package.name)
+        except ImportError:
+            # ImportErrors are non-fatal because model extensions are optional.
+            pass
