761110d869049638c61e03be46c8b612437c7f35
Dominik George <dominik.george@teckids.org>
2019-11-12 23:23:08 +0100

    Remvoe photo generation for now.

--- biscuit/core/anonymizers.py
+++ biscuit/core/anonymizers.py
@@ -1,25 +1,6 @@
-import requests
-
-from django.core.files.base import ContentFile
 from hattori.base import BaseAnonymizer, faker
 
 from .models import Person
-
-
-def get_photo(**kwargs):
-    req = requests.get(
-        'https://thispersondoesnotexist.com/image',
-        headers={
-            'User-Agent': faker.firefox()
-        }
-    )
-
-    # Needed to get around hattori trying to truncate to max_length
-    class _ContentFile(ContentFile):
-        def __getitem__(self, key):
-            return self
-
-    return _ContentFile(req.content, faker.file_name(extension='jpg'))
 
 
 class PersonAnonymizer(BaseAnonymizer):
@@ -38,5 +19,5 @@ class PersonAnonymizer(BaseAnonymizer):
         ('mobile_number', ''),
         ('email', faker.email),
         ('date_of_birth', lambda **kwargs: faker.date_of_birth(minimum_age=8, maximum_age=66, **kwargs)),
-        ('photo', get_photo)
+        ('photo', '')
     ]
