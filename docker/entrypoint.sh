69fbe89555e6075c2089d1d498abeb3890bb16ab
Dominik George <dominik.george@teckids.org>
2019-11-14 12:38:38 +0100

    ALlow using all $POSTGRES_* variables to ease combination with postgres Docker image.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 GUNICORN_BIND=${GUNICORN_BIND:-0.0.0.0:8000}
-POSTGRES_HOST=${BISCUIT_database__host:-127.0.0.1}
-POSTGRES_PORT=${BISCUIT_database__port:-5432}
+POSTGRES_HOST=${POSTGRES_HOST:-127.0.0.1}
+POSTGRES_PORT=${POSTGRES_PORT:-5432}
+POSTGRES_DB=${POSTGRES_DB:-biscuit}
+POSTGRES_USER=${POSTGRES_USER:-biscuit}
 
-[[ -n $POSTGRES_PASSWORD ]] && BISCUIT_database__password=$POSTGRES_PASSWORD
+BISCUIT_database__host=${BISCUIT_database__host:-$POSTGRES_HOST}
+BISCUIT_database__port=${BISCUIT_database__port:-$POSTGRES_PORT}
+BISCUIT_database__name=${BISCUIT_database__name:-$POSTGRES_DB}
+BISCUIT_database__user=${BISCUIT_database__user:-$POSTGRES_USER}
+BISCUIT_database__password=${BISCUIT_database__password:-$POSTGRES_PASSWORD}
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+while ! nc -z $BISCUIT_database__host $BISCUIT_database__port; do
     sleep 0.1
 done
 
