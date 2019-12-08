65371191de6c8a962ee045e654c1b2dd52514e3b
Dominik George <dominik.george@teckids.org>
2019-12-08 18:59:53 +0100

    Adjust line-spacing

--- biscuit/core/admin.py
+++ biscuit/core/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
+
 from .models import Group, Person, School, SchoolTerm
 
 
 admin.site.register(Person)
-
-
 admin.site.register(Group)
-
-
 admin.site.register(School)
-
-
 admin.site.register(SchoolTerm)
