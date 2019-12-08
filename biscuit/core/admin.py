3672600094dd51fcc8abaa9d5a7eefb4c75c7e7a
Tom Teichler <tom.teichler@teckids.org>
2019-12-08 18:55:40 +0100

    Simplify admin.py.

--- biscuit/core/admin.py
+++ biscuit/core/admin.py
@@ -2,29 +2,13 @@ from django.contrib import admin
 from .models import Group, Person, School, SchoolTerm
 
 
-class PersonAdmin(admin.ModelAdmin):
-    pass
+admin.site.register(Person)
 
 
-admin.site.register(Person, PersonAdmin)
+admin.site.register(Group)
 
 
-class GroupAdmin(admin.ModelAdmin):
-    pass
+admin.site.register(School)
 
 
-admin.site.register(Group, GroupAdmin)
-
-
-class SchoolAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(School, SchoolAdmin)
-
-
-class SchoolTermAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(SchoolTerm, SchoolTermAdmin)
+admin.site.register(SchoolTerm)
