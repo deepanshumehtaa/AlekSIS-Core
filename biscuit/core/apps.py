ff8ace5070055845f4c8daab3332d563251a98c4
Dominik George <dominik.george@teckids.org>
2019-12-08 20:33:20 +0100

    Move Yubikey validation servcie creation to migration
    Closes #111.

--- biscuit/core/apps.py
+++ biscuit/core/apps.py
@@ -2,7 +2,7 @@ from glob import glob
 import os
 from warnings import warn
 
-from django.apps import AppConfig, apps
+from django.apps import AppConfig
 from django.conf import settings
 from django.db.utils import ProgrammingError
 
@@ -19,14 +19,5 @@ class CoreConfig(AppConfig):
                 # Ignore because old is better than nothing
                 pass  # noqa
 
-    def setup_data(self) -> None:
-        try:
-            apps.get_model('otp_yubikey', 'ValidationService').objects.update_or_create(
-                name='default', defaults={'use_ssl': True, 'param_sl': '', 'param_timeout': ''}
-            )
-        except ProgrammingError:
-            warn('Yubikey validation service could not be created yet. If you are currently in a migration, this is expected.')
-
     def ready(self) -> None:
         self.clean_scss()
-#        self.setup_data()
