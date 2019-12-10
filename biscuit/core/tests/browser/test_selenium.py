e38ea1ffd1eef4c2944b8aa22a0a71319606839a
Dominik George <dominik.george@teckids.org>
2019-12-10 23:06:13 +0100

    [CI] Introduce Selenium tests against Docker

---
+++ biscuit/core/tests/browser/test_selenium.py
@@ -0,0 +1,17 @@
+import sys
+
+import pytest
+
+if '--driver' not in sys.argv:
+    pytest.skip('Selenium driver not configured', allow_module_level=True)
+
+@pytest.fixture
+def chrome_options(chrome_options):
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    return chrome_options
+
+def test_index(selenium):
+    selenium.get('http://app:8000/')
+    selenium.save_screenshot('screenshots/index.png')
