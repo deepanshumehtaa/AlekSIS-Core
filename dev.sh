813724f51bc4ee6866ccb9ebc9440f870ea970fe
Dominik George <dominik.george@teckids.org>
2020-01-02 12:27:23 +0100

    [Dev] Update poetry.lock before install

--- dev.sh
+++ dev.sh
@@ -9,10 +9,11 @@ case "$1" in
         set -e
 	cd "$(dirname "$0")"
 	remove_pip_metadata
+	poetry lock
 	poetry install
 	for d in apps/official/*; do
 	    remove_pip_metadata
-	    poetry run sh -c "cd $d; poetry install"
+	    poetry run sh -c "cd $d; poetry lock; poetry install"
 	done
 	remove_pip_metadata
 	poetry run ./manage.py compilemessages
