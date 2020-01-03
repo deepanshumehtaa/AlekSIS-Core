5f9b08aa16f9d28d99176c3309376a35d137731f
Dominik George <dominik.george@teckids.org>
2020-01-03 17:32:40 +0100

    Remove cruft that will not be merged here.
    Cf. #134

--- biscuit/core/debug/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.debugging_tool),
-    path("logs/", views.debugging_tool, name="debug_logs")
-]
--- react/babel.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-npx babel --watch src/ --out-dir ../schoolapps/static/js/ --presets react-app/prod
