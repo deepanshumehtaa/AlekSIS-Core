74d8f93287a3d88d134785fa80ab4161ca92befc
mirabilos <t.glaser@tarent.de>
2020-01-03 14:09:25 +0100

    Drop old GPL mentions

--- react/src/rebus.js
+++ react/src/rebus.js
@@ -1,35 +1,3 @@
-/*
- * This file is part of SchoolApps.
- *
- * SchoolApps is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License.
- *
- * SchoolApps is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SchoolApps.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- * This file is part of SchoolApps.
- *
- * SchoolApps is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License.
- *
- * SchoolApps is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SchoolApps.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 const OPTIONS_ONLINE_COMMON = [
     "Portal ist nicht erreichbar",
     "Fehlermeldung(en) tauchen auf",
