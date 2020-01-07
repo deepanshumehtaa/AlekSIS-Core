d1b725b04a51c1dc28c36c6050be8adede19e9b3
Jonathan Weth <git@jonathanweth.de>
2020-01-07 19:43:49 +0100

    Fix person_required with support for anonymous users
    Addition to MR #110

--- aleksis/core/decorators.py
+++ aleksis/core/decorators.py
@@ -2,6 +2,8 @@ from typing import Callable
 
 from django.contrib.auth.decorators import login_required, user_passes_test
 
+from aleksis.core.util.core_helpers import has_person
+
 
 def admin_required(function: Callable = None) -> Callable:
     actual_decorator = user_passes_test(lambda u: u.is_active and u.is_superuser)
@@ -11,5 +13,5 @@ def admin_required(function: Callable = None) -> Callable:
 def person_required(function: Callable = None) -> Callable:
     """ Requires a logged-in user which is linked to a person. """
 
-    actual_decorator = user_passes_test(lambda u: not u.is_anonymous and u.person is not None)
+    actual_decorator = user_passes_test(has_person)
     return actual_decorator(login_required(function))
