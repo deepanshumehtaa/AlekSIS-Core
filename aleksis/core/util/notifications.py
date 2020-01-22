d0ba42f099c88a6544807a1cdd73b0b086d30856
Dominik George <dominik.george@teckids.org>
2020-01-22 17:13:33 +0100

    Implement SMS sending for notifications

--- aleksis/core/util/notifications.py
+++ aleksis/core/util/notifications.py
@@ -1,13 +1,31 @@
 """ Utility code for notification system """
 
+from typing import Sequence
+
 from django.conf import settings
+from django.template.loader import get_template
 from django.utils.translation import gettext_lazy as _
 
 from constance import config
 from templated_email import send_templated_mail
+try:
+    from twilio.rest import Client as TwilioClient
+except ImportError:
+    TwilioClient = None
 
 from ..models import Notification
 from .core_helpers import celery_optional
+
+
+def send_templated_sms(template_name: str, from_number: str, recipient_list: Sequence[str], context: dict):
+    """ Render a plan-text template and send via SMS to all recipients. """
+
+    template = get_template(template_name)
+    text = template.render(context)
+
+    client = TwilioClient(settings.TWILIO_SID, settings.TWILIO_TOKEN)
+    for recipient in recipient_list:
+        client.messages.create(body=text, to=recipient, from_=from_number)
 
 
 def _send_notification_email(notification: Notification, template: str = "notification") -> None:
@@ -19,6 +37,19 @@ def _send_notification_email(notification: Notification, template: str = "notifi
         template_name=template,
         from_email=config.MAIL_OUT,
         recipient_list=[notification.person.email],
+        context=context,
+    )
+
+
+def _send_notification_sms(notification: Notification, template: str = "sms/notification.txt") -> None:
+    context = {
+        "notification": notification,
+        "notification_user": notification.person.adressing_name,
+    }
+    send_templated_sms(
+        template_name=template,
+        from_number=settings.TWILIO_CALLER_ID,
+        recipient_list=[notification.person.mobile_number],
         context=context,
     )
 
