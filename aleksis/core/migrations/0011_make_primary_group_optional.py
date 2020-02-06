e95ebdb4cca390c2ae9a8094ad72e02fe17e6cf6
Dominik George <dominik.george@teckids.org>
2020-02-06 14:44:12 +0100

    Fix copy/paste mistakes in migration dependencies

--- aleksis/core/migrations/0011_make_primary_group_optional.py
+++ aleksis/core/migrations/0011_make_primary_group_optional.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0009_dashboard_widget.py'),
+        ('core', '0009_dashboard_widget'),
     ]
 
     operations = [
