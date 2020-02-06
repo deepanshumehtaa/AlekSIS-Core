0392be85e3ed508685ab74d3ff57cc8301ebcb37
Dominik George <dominik.george@teckids.org>
2020-02-06 14:21:28 +0100

    Remove left-over ready() method

--- aleksis/core/apps.py
+++ aleksis/core/apps.py
@@ -1,3 +1,5 @@
+from typing import Any, List, Optional, Tuple
+
 from .signals import clean_scss
 from .util.apps import AppConfig
 
@@ -5,9 +7,6 @@ from .util.apps import AppConfig
 class CoreConfig(AppConfig):
     name = "aleksis.core"
     verbose_name = "AlekSIS — The Free School Information System"
-
-    def ready(self) -> None:
-        super().ready()
 
     def config_updated(self, *args, **kwargs) -> None:
         clean_scss()
