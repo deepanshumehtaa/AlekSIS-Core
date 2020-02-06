9ce1e718ecbe1be45a879bd60d11f73659d1d5f8
Dominik George <dominik.george@teckids.org>
2020-02-06 14:23:46 +0100

    Register system checks of all apps when ready

--- aleksis/core/util/apps.py
+++ aleksis/core/util/apps.py
@@ -1,5 +1,5 @@
 from importlib import import_module
-from typing import Any, Optional
+from typing import Any, List, Optional, Tuple
 
 import django.apps
 from django.db.models.signals import post_migrate, pre_migrate
@@ -30,6 +30,15 @@ class AppConfig(django.apps.AppConfig):
         # Getting an app ready means it should look at its config once
         self.config_updated()
 
+        # Register system checks of this app
+        try:
+            import_module(
+                ".".join(self.__class__.__module__.split(".")[:-1] + ["checks"])
+            )
+        except ImportError:
+            # ImportErrors are non-fatal because checks are optional.
+            pass
+
     def config_updated(
         self,
         key: Optional[str] = "",
@@ -50,7 +59,7 @@ class AppConfig(django.apps.AppConfig):
         interactive: bool,
         using: str,
         plan: List[Tuple],
-        apps: django.apps.Apps,
+        apps: django.apps,
     ) -> None:
         """ Called on every app instance before its models are migrated
 
@@ -65,7 +74,7 @@ class AppConfig(django.apps.AppConfig):
         interactive: bool,
         using: str,
         plan: List[Tuple],
-        apps: django.apps.Apps,
+        apps: django.apps,
     ) -> None:
         """ Called on every app instance after its models have been migrated
 
