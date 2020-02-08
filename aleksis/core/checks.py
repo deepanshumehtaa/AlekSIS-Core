cc459120ceab9983d7a80385e5775ba5b57c82e5
Jonathan Weth <git@jonathanweth.de>
2020-02-08 11:12:54 +0100

    Fix import in check_app_configs_base_class

--- aleksis/core/checks.py
+++ aleksis/core/checks.py
@@ -15,7 +15,7 @@ def check_app_configs_base_class(
     results = []
 
     if app_configs is None:
-        app_configs = django.apps.get_app_configs()
+        app_configs = django.apps.apps.get_app_configs()
 
     for app_config in app_configs:
         if app_config.name.startswith("aleksis.apps.") and not isinstance(app_config, AppConfig):
