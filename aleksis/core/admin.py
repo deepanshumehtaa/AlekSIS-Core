cb8c19d727b87e02af33fa64ff1d8e1ef6cbc538
Jonathan Weth <git@jonathanweth.de>
2020-02-16 14:49:20 +0100

    Improve announcement model
    - Add __str__
    - Add verbose names
    - Make description optional
    - Add new default for valid_until

--- aleksis/core/admin.py
+++ aleksis/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Group, Person, School, SchoolTerm, Activity, Notification
+from .models import Group, Person, School, SchoolTerm, Activity, Notification, Announcement
 
 admin.site.register(Person)
 admin.site.register(Group)
@@ -8,3 +8,4 @@ admin.site.register(School)
 admin.site.register(SchoolTerm)
 admin.site.register(Activity)
 admin.site.register(Notification)
+admin.site.register(Announcement)
