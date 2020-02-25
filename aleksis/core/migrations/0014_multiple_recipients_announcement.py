49ae8f8bf55f964971ee31f1a42373f302082b41
Jonathan Weth <git@jonathanweth.de>
2020-02-25 16:26:11 +0100

    Remove merge migration 0014 and move 0013_multiple_recipients_announcement to 0014_multiple_recipients_announcement instead

--- aleksis/core/migrations/0014_multiple_recipients_announcement.py
+++ aleksis/core/migrations/0014_multiple_recipients_announcement.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('contenttypes', '0002_remove_content_type_name'),
-        ('core', '0012_announcement'),
+        ('core', '0013_extensible_model_as_default'),
     ]
 
     operations = [
