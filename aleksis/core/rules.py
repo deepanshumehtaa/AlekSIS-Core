cd94541ae5dbf08a5f267154a131fd9a16648a3c
Hangzhi <hangzhi@protonmail.com>
2020-04-18 13:43:46 +0200

    Add rules that were already mentioned in template
    Add rules that were already mentioned in template
    
    Add rules that were already mentioned in template

--- aleksis/core/rules.py
+++ aleksis/core/rules.py
@@ -24,6 +24,24 @@ view_person_predicate = has_person_predicate & (
 )
 add_perm("core.view_person", view_person_predicate)
 
+# View person address
+view_address_predicate = has_person_predicate & (
+    has_global_perm("core.view_address") | has_object_perm("core.view_address") | is_person
+)
+add_perm("core.view_address", view_address_predicate)
+
+# View person contact details
+view_contact_details_predicate = has_person_predicate & (
+    has_global_perm("core.view_contact_details") | has_object_perm("core.view_contact_details") | is_person
+)
+add_perm("core.view_contact_details", view_contact_details_predicate)
+
+# View person photo
+view_photo_predicate = has_person_predicate & (
+    has_global_perm("core.view_photo") | has_object_perm("core.view_photo") | is_person
+)
+add_perm("core.view_photo", view_photo_predicate)
+
 # Change person
 change_person_predicate = has_person_predicate & (
     has_global_perm("core.change_person") | has_object_perm("core.change_person")
@@ -37,7 +55,7 @@ view_groups_predicate = has_person_predicate & (
 add_perm("core.view_groups", view_groups_predicate)
 
 # View group
-view_group_predicate = has_person_predicate &(
+view_group_predicate = has_person_predicate & (
     has_global_perm("core.view_group") | has_object_perm("core.view_group")
 )
 add_perm("core.view_group", view_group_predicate)
