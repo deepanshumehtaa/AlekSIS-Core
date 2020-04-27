87ef531d21c73b067012849cafa97c3fa3f2d4e8
Tom Teichler <tom.teichler@teckids.org>
2020-04-27 15:12:37 +0200

    Enable french

--- aleksis/core/settings.py
+++ aleksis/core/settings.py
@@ -262,6 +262,7 @@ AUTHENTICATION_BACKENDS.append("django.contrib.auth.backends.ModelBackend")
 LANGUAGES = [
     ("de", _("German")),
     ("en", _("English")),
+    ("fr", _("French")),
 ]
 LANGUAGE_CODE = _settings.get("l10n.lang", "en")
 TIME_ZONE = _settings.get("l10n.tz", "UTC")
