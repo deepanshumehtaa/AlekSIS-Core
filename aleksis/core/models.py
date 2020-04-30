1d66da178f46c0ec4cdcbc9f3154f2113675d6cc
Tom Teichler <tom.teichler@teckids.org>
2020-04-30 21:30:02 +0200

    Fix model fields

--- aleksis/core/models.py
+++ aleksis/core/models.py
@@ -528,12 +528,12 @@ class GlobalPermissions(ExtensibleModel):
 
 
 class SitePreferenceModel(PerInstancePreferenceModel):
-    instance = models.ForeignKey(Site)
+    instance = models.ForeignKey(Site, on_delete=models.CASCADE)
 
 
 class PersonPreferenceModel(PerInstancePreferenceModel):
-    instance = models.ForeignKey(Person)
+    instance = models.ForeignKey(Person, on_delete=models.CASCADE)
 
 
 class GroupPreferenceModel(PerInstancePreferenceModel):
-    instance = models.ForeignKey(Group)
+    instance = models.ForeignKey(Group, on_delete=models.CASCADE)
