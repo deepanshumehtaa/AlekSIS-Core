31f4cf58ba577a0a5d91f2424fb3cbfc42ece66b
Tom Teichler <tom.teichler@teckids.org>
2020-06-04 19:33:46 +0200

    Style delete button

--- aleksis/core/tables.py
+++ aleksis/core/tables.py
@@ -32,7 +32,11 @@ class AdditionalFieldsTable(tables.Table):
 
     title = tables.LinkColumn("edit_additional_field_by_id", args=[A("id")])
     delete = tables.LinkColumn(
-        "delete_additional_field_by_id", args=[A("id")], verbose_name=_("Delete"), text=_("Delete")
+        "delete_additional_field_by_id",
+        args=[A("id")],
+        verbose_name=_("Delete"),
+        text=_("Delete"),
+        attrs={"a": {"class": "btn-flat waves-effect waves-red"}},
     )
 
 
