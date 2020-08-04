6fceb73324e6fd44dc51a4f50794488bb6fb5d17
Jonathan Weth <git@jonathanweth.de>
2020-08-04 20:23:53 +0200

    Fix options for dbbackup

--- aleksis/core/tasks.py
+++ aleksis/core/tasks.py
@@ -19,11 +19,21 @@ def send_notification(notification: int, resend: bool = False) -> None:
 def backup_data() -> None:
     """Backup database and media using django-dbbackup."""
     # Assemble command-line options for dbbackup management command
-    db_options = "-z " * settings.DBBACKUP_COMPRESS_DB + "-e" * settings.DBBACKUP_ENCRYPT_DB
+    db_options = (
+        ["-z"]
+        if settings.DBBACKUP_COMPRESS_DB
+        else [] + ["-e"]
+        if settings.DBBACKUP_ENCRYPT_DB
+        else []
+    )
     media_options = (
-        "-z " * settings.DBBACKUP_COMPRESS_MEDIA + "-e" * settings.DBBACKUP_ENCRYPT_MEDIA
+        ["-z "]
+        if settings.DBBACKUP_COMPRESS_MEDIA
+        else [] + "-e"
+        if settings.DBBACKUP_ENCRYPT_MEDIA
+        else []
     )
 
     # Hand off to dbbackup's management commands
-    management.call_command("dbbackup", db_options)
-    management.call_command("mediabackup", media_options)
+    management.call_command("dbbackup", *db_options)
+    management.call_command("mediabackup", *media_options)
