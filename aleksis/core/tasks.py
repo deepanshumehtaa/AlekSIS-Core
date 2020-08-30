3c68e88b0619b2294923236738139456a79d1a25
Jonathan Weth <git@jonathanweth.de>
2020-08-30 15:51:01 +0200

    Add cleanup option for db and media backup

--- aleksis/core/tasks.py
+++ aleksis/core/tasks.py
@@ -19,12 +19,21 @@ def send_notification(notification: int, resend: bool = False) -> None:
 def backup_data() -> None:
     """Backup database and media using django-dbbackup."""
     # Assemble command-line options for dbbackup management command
-    db_options = (["-z"] if settings.DBBACKUP_COMPRESS_DB else []) + (
-        ["-e"] if settings.DBBACKUP_ENCRYPT_DB else []
-    )
-    media_options = (["-z"] if settings.DBBACKUP_COMPRESS_MEDIA else []) + (
-        ["-e"] if settings.DBBACKUP_ENCRYPT_MEDIA else []
-    )
+    db_options = []
+    if settings.DBBACKUP_COMPRESS_DB:
+        db_options.append("-z")
+    if settings.DBBACKUP_ENCRYPT_DB:
+        db_options.append("-e")
+    if settings.DBBACKUP_CLEANUP_DB:
+        db_options.append("-c")
+
+    media_options = []
+    if settings.DBBACKUP_COMPRESS_MEDIA:
+        media_options.append("-z")
+    if settings.DBBACKUP_ENCRYPT_MEDIA:
+        media_options.append("-e")
+    if settings.DBBACKUP_CLEANUP_MEDIA:
+        media_options.append("-c")
 
     # Hand off to dbbackup's management commands
     management.call_command("dbbackup", *db_options)
