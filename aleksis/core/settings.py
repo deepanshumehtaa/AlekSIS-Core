854b15e6322fe186308a7d59335fc76f3b5c5b7e
Dominik George <dominik.george@teckids.org>
2021-01-25 23:43:36 +0100

    Install django-notifs

--- aleksis/core/settings.py
+++ aleksis/core/settings.py
@@ -110,6 +110,7 @@ INSTALLED_APPS = [
     "dynamic_preferences.users.apps.UserPreferencesConfig",
     "impersonate",
     "two_factor",
+    "notifications",
     "material",
     "pwa",
     "ckeditor",
