77ed43d87e04b45102f22fc94faaa0d7844b0b27
Dominik George <dominik.george@teckids.org>
2021-02-12 16:59:20 +0100

    Trick autoreloader to think we are django-admin in aleksis-admin
    Follow-up on 2f7ccc1d2db7a93e0485ba2db98f62eaf35c113f

--- aleksis/core/__main__.py
+++ aleksis/core/__main__.py
@@ -3,13 +3,14 @@
 import os
 import sys
 
+import django.__main__
 from django.core.management import execute_from_command_line
 
 
 def aleksis_cmd():
     """Run django-admin command with correct settings path."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aleksis.core.settings")
-    sys.argv[0] = os.path.dirname(__file__)
+    sys.argv[0] = django.__main__.__file__
     execute_from_command_line(sys.argv)
 
 
