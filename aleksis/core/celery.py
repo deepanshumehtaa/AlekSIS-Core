d69e7ac6253c6cdd36d82beff7b4f2b6c2afc447
Dominik George <dominik.george@teckids.org>
2021-02-17 11:18:14 +0100

    Revert "Replace default app Task with a task that runs on_commit"
    This reverts commit c82be665bffa8eec7269bcfd94b3b8129b518b1b.

--- aleksis/core/celery.py
+++ aleksis/core/celery.py
@@ -1,7 +1,5 @@
 import os
 
-from django.db import transaction
-
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aleksis.core.settings")
@@ -9,14 +7,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aleksis.core.settings")
 app = Celery("aleksis")  # noqa
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
-
-
-class OnCommitTask(app.Task):
-    """Task that is delayed at least until the current transaction commits."""
-    def delay(self, *args, **kwargs):
-        def _real_delay():
-            return super().delay(*args, **kwargs)
-        transaction.on_commit(_real_delay)
-
-
-app.Task = OnCommitTask
