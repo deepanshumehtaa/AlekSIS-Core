a632a5a8553d017507013525127382d608ff0f11
Tom Teichler <tom.teichler@teckids.org>
2021-03-21 21:57:39 +0100

    Check if s3 is enabled for staticfiles

--- aleksis/core/settings.py
+++ aleksis/core/settings.py
@@ -787,22 +787,24 @@ PROMETHEUS_EXPORT_MIGRATIONS = False
 
 if _settings.get("storage.s3.enabled", False):
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
+
+    if _settings.get("storage.s3.static.enabled", False):
+        STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
+        AWS_STORAGE_BUCKET_NAME_STATIC = _settings.get("storage.s3.static.bucket_name", "")
+        AWS_S3_MAX_AGE_SECONDS_CACHED_STATIC = _settings.get(
+            "storage.s3.static.max_age_seconds", 31536000
+        )
 
     AWS_REGION = _settings.get("storage.s3.region", "")
     AWS_ACCESS_KEY_ID = _settings.get("storage.s3.access_key_id", "")
     AWS_SECRET_ACCESS_KEY = _settings.get("storage.s3.secret_key", "")
     AWS_SESSION_TOKEN = _settings.get("storage.s3.session_token", "")
     AWS_STORAGE_BUCKET_NAME = _settings.get("storage.s3.bucket_name", "")
-    AWS_STORAGE_BUCKET_NAME_STATIC = _settings.get("storage.s3.bucket_name_static", "")
     AWS_S3_ADDRESSING_STYLE = _settings.get("storage.s3.addressing_style", "auto")
     AWS_S3_ENDPOINT_URL = _settings.get("storage.s3.endpoint_url", "")
     AWS_S3_KEY_PREFIX = _settings.get("storage.s3.key_prefix", "")
     AWS_S3_BUCKET_AUTH = _settings.get("storage.s3.bucket_auth", True)
     AWS_S3_MAX_AGE_SECONDS = _settings.get("storage.s3.max_age_seconds", 3600)
-    AWS_S3_MAX_AGE_SECONDS_CACHED_STATIC = _settings.get(
-        "storage.s3.max_age_seconds_cached_static", 31536000
-    )
     AWS_S3_PUBLIC_URL = _settings.get("storage.s3.public_url", "")
     AWS_S3_REDUCED_REDUNDANCY = _settings.get("storage.s3.reduced_redundancy", False)
     AWS_S3_CONTENT_DISPOSITION = _settings.get("storage.s3.content_disposition", "")
