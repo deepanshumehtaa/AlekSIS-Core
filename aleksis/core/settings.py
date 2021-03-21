0ab6dc3670772eda919b19b2a87e45175085c994
Tom Teichler <tom.teichler@teckids.org>
2021-03-21 21:08:12 +0100

    Use django-storages instead of django-s3-storage

--- aleksis/core/settings.py
+++ aleksis/core/settings.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = [
     "polymorphic",
     "django_global_request",
     "dbbackup",
+    "storages",
     "django_celery_beat",
     "django_celery_results",
     "celery_progress",
@@ -785,13 +786,17 @@ MEDIABACKUP_CHECK_SECONDS = _settings.get("backup.media.check_seconds", 7200)
 PROMETHEUS_EXPORT_MIGRATIONS = False
 
 if _settings.get("storage.s3.enabled", False):
-    DEFAULT_FILE_STORAGE = "django_s3_storage.storage.S3Storage"
-    STATICFILES_STORAGE = "django_s3_storage.storage.StaticS3Storage"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
 
-    AWS_REGION = _settings.get("storage.s3.region", "")
+    if _settings.get("storage.s3.use_aws", False):
+        AWS_REGION = _settings.get("storage.s3.region", "")
+
     AWS_ACCESS_KEY_ID = _settings.get("storage.s3.access_key_id", "")
+    AWS_SECRET_ACCESS_KEY = _settings.get("storage.s3.secret_key", "")
     AWS_SESSION_TOKEN = _settings.get("storage.s3.session_token", "")
-    AWS_S3_BUCKET_NAME = _settings.get("storage.s3.bucket_name", "")
+    AWS_STORAGE_BUCKET_NAME = _settings.get("storage.s3.bucket_name", "")
+    AWS_STORAGE_BUCKET_NAME_STATIC = _settings.get("storage.s3.bucket_name_static", "")
     AWS_S3_ADDRESSING_STYLE = _settings.get("storage.s3.addressing_style", "auto")
     AWS_S3_ENDPOINT_URL = _settings.get("storage.s3.endpoint_url", "")
     AWS_S3_KEY_PREFIX = _settings.get("storage.s3.key_prefix", "")
