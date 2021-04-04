67789f0135f9e2ba45728fd1ad3afda7cc7eb7da
Tom Teichler <tom.teichler@teckids.org>
2021-04-04 21:12:25 +0200

    Add test job

---
+++ aleksis/core/tests/models/test_openid.py
@@ -0,0 +1,15 @@
+import pytest
+from oauth2_provider.models import Application
+
+pytestmark = pytest.mark.django_db
+
+
+def test_application_create():
+    _application = Application.objects.create(
+        name="Test Application",
+        redirect_uris="https://example.com/redirect https://example.de/redirect",
+        client_type="public",
+        authorization_grant_type="authorization-code",
+    )
+
+    assert _application.name == "Test Application"
