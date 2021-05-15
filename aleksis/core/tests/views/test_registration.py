0b3069be2b02e1b8cd39f594121b2edffadf5d97
Tom Teichler <tom.teichler@teckids.org>
2021-05-15 12:36:38 +0200

    Fix url

--- aleksis/core/tests/views/test_registration.py
+++ aleksis/core/tests/views/test_registration.py
@@ -7,7 +7,7 @@ pytestmark = pytest.mark.django_db
 
 
 def test_registration_page(client):
-    response = client.get("/accounts/signup")
+    response = client.get("/accounts/signup/")
 
     assert response.status_code == 200
     assert "Already have an account?" in response.content.decode("utf-8")
