b1852eeb231ddeb27fe848d9724e80e226849e4f
Jonathan Weth <git@jonathanweth.de>
2021-05-15 18:41:32 +0200

    Fix migration order

--- aleksis/core/migrations/0017_dashboardwidget_broken.py
+++ aleksis/core/migrations/0017_dashboardwidget_broken.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0009_default_dashboard'),
+        ('core', '0016_taskuserassignment'),
     ]
 
     operations = [
