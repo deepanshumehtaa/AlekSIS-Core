0a1ac95e67ecd9a8023b3ae26a108f0fd95f161e
Jonathan Weth <git@jonathanweth.de>
2021-05-15 17:36:10 +0200

    Fix backup health check if backup dir doesn't exist

--- aleksis/core/health_checks.py
+++ aleksis/core/health_checks.py
@@ -34,6 +34,9 @@ class BaseBackupHealthCheck(BaseHealthCheckBackend):
     def check_status(self):
         storage = get_storage()
         backups = storage.list_backups(content_type=self.content_type)
+        if not storage.storage.exists(""):
+            self.add_error(_("The backup folder doesn't exist."))
+            return
         if backups:
             last_backup = backups[-1]
             last_backup_time = dbbackup_utils.filename_to_date(last_backup)
