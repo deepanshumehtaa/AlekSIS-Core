669beffee3840d7fda33baf5729e2b203e77ff3e
Jonathan Weth <git@jonathanweth.de>
2021-08-24 22:22:06 +0200

    Bump version to 2.0rc5

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ author = "AlekSIS team"
 # The short X.Y version
 version = "2.0"
 # The full version, including alpha/beta/rc tags
-release = "2.0rc3"
+release = "2.0rc5"
 
 
 # -- General configuration ---------------------------------------------------
