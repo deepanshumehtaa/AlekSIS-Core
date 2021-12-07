13175d3a00e7ef365b74909cdaa6ff9cd66f0ee3
Dominik George <dominik.george@teckids.org>
2021-12-07 11:00:36 +0100

    [Django 4] Align STATIC_URL and MEDIA_URL with new template
    This was actually a bug.

--- aleksis/core/settings.py
+++ aleksis/core/settings.py
@@ -487,8 +487,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 
-STATIC_URL = _settings.get("static.url", "/static/")
-MEDIA_URL = _settings.get("media.url", "/media/")
+STATIC_URL = _settings.get("static.url", "static/")
+MEDIA_URL = _settings.get("media.url", "media/")
 
 LOGIN_REDIRECT_URL = "index"
 LOGOUT_REDIRECT_URL = "index"
